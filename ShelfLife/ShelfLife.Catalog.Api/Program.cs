fa47890a76116dc850d99998693b0724d46ea022
Rostyslav Palamar <Ayncuin@gmail.com>
2026-07-10 00:13:39 +0300

    Added and applied configuration for the docker files

--- ShelfLife/ShelfLife.Catalog.Api/Program.cs
+++ ShelfLife/ShelfLife.Catalog.Api/Program.cs
@@ -4,7 +4,7 @@ using ShelfLife.Catalog.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter()));
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
